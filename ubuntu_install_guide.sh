6d928e8d1eab6dde7c0857afac8b0821b3d17be2
huangjin <374610406@qq.com>
2014-12-28 23:34:09 +0800

    Update ubuntu_install_guide.sh

--- ubuntu_install_guide.sh
+++ ubuntu_install_guide.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+# -y 参数表示不需要确认直接安装
 cd ~
 sudo apt-get update
 
+sudo apt-get install git -y
 sudo apt-get install gcc -y
 sudo apt-get install g++ -y
 sudo apt-get install vim -y
@@ -20,6 +22,19 @@ sudo apt-get install proxychains -y
 
 sudo apt-get install tilda -y
 sudo apt-get install ipython -y
-sudo apt-get install uget -y
-sudo apt-get install smplayer -y
+sudo apt-get install uget aria2 -y
+sudo apt-get install mplayer smplayer -y
+sudo apt-get install gimp -y
+sudo apt-get install fcitx -y
 
+#为知笔记
+sudo add-apt-repository ppa:wiznote-team
+sudo apt-get update
+sudo apt-get install wiznote -y
+
+#音乐和歌词
+sudo apt-get install exaile osd-lyrics -y
+#截图
+sudo apt-get install shutter -y
+#终端增强
+#sudo apt-get install terminator
