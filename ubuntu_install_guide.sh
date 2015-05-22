3a8c7be5b40a060a433a2c8cf211246dd7de53a3
hj <hj@dantangfan.com>
2015-05-22 11:52:24 +0800

    s

--- ubuntu_install_guide.sh
+++ ubuntu_install_guide.sh
@@ -3,19 +3,18 @@
 # -y 参数表示不需要确认直接安装
 cd ~
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 sudo apt-get install git -y
 sudo apt-get install gcc -y
 sudo apt-get install g++ -y
 sudo apt-get install vim -y
 sudo apt-get install emma -y
 sudo apt-get install virtualbox -y
-sudo apt-get install wireshark
+sudo apt-get install wireshark -y
 sudo apt-get install wget -y
 sudo apt-get install curl -y
 
 #charles
-
 sudo apt-get install tilda -y
 sudo apt-get install ipython -y
 sudo apt-get install uget aria2 -y
@@ -36,3 +35,5 @@ sudo apt-get install shutter -y
 sudo apt-get install kchmviewer -y
 #sudo apt-get install chmsee
 
+sudo apt-get install mysql-server -y
+sudo apt-get install mongodb -y
