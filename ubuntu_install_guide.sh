1b695ee1ae7419eac70add8e9e4e0ae0314b3304
dantangfan <374610406@qq.com>
2016-04-24 18:05:43 +0800

    enheng

--- ubuntu_install_guide.sh
+++ ubuntu_install_guide.sh
@@ -8,32 +8,35 @@ sudo apt-get install git -y
 sudo apt-get install gcc -y
 sudo apt-get install g++ -y
 sudo apt-get install vim -y
-sudo apt-get install emma -y
+#sudo apt-get install emma -y
 sudo apt-get install virtualbox -y
 sudo apt-get install wireshark -y
 sudo apt-get install wget -y
 sudo apt-get install curl -y
-
-#charles
 sudo apt-get install tilda -y
 sudo apt-get install ipython -y
-sudo apt-get install uget aria2 -y
+#sudo apt-get install uget aria2 -y
 sudo apt-get install mplayer smplayer -y
-sudo apt-get install gimp -y
+#sudo apt-get install gimp -y
 sudo apt-get install fcitx -y
 
 #音乐和歌词
-sudo apt-get install exaile osd-lyrics -y
+#sudo apt-get install exaile osd-lyrics -y
 #音乐剪辑
-sudo apt-get install audacity -y
+#sudo apt-get install audacity -y
 #截图
 sudo apt-get install shutter -y
 #终端增强
-#sudo apt-get install terminator -y
+sudo apt-get install terminator -y
 
 #chm阅读
-sudo apt-get install kchmviewer -y
+#sudo apt-get install kchmviewer -y
 #sudo apt-get install chmsee
 
 sudo apt-get install mysql-server -y
 sudo apt-get install mongodb -y
+sudo apt-get install python-dev -y
+sudo apt-get install nginx -y
+sudo apt-get install ctags -y
+# rabbitmq http://www.rabbitmq.com/install-debian.html
+
